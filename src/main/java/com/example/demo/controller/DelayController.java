de2bace52db4d1060d0cf6965c4647d1ff7353d7
WH <wh775349543@gmail.com>
2021-09-23 09:54:40 +0800

    update

--- src/main/java/com/example/demo/controller/DelayController.java
+++ src/main/java/com/example/demo/controller/DelayController.java
@@ -1,5 +1,9 @@
 package com.example.demo.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.example.demo.entity.DelayJob;
+import com.example.demo.entity.Job;
+import com.example.demo.serivce.JobService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +16,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class DelayController {
 
+    private final JobService jobService;
 
+    @RequestMapping("/add")
+    public String addDefJob(Job request){
+        DelayJob delayJob = jobService.addDefJob(request);
+        return JSON.toJSONString(delayJob);
+    }
+
+    @RequestMapping("/pop")
+    public String getProcessJob(String topic){
+        Job process = jobService.getProcessJob(topic);
+        return JSON.toJSONString(process);
+    }
+
+    @RequestMapping("/finish")
+    public String finishJob(Long jobId){
+        jobService.finishJob(jobId);
+        return "success";
+    }
+
+    @RequestMapping("/delete")
+    public String deleteJob(Long jobId){
+        jobService.deleteJob(jobId);
+        return "success";
+    }
 
 }
